a9833b1273b21192a5c7fed27080ce079ede7905
Catminusminus <37803616+Catminusminus@users.noreply.github.com>
2021-08-05 00:15:10 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+};
